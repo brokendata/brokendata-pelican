df100473ffe22b60a565d3588fe282f899df4a14
ryan <brokendata@github.com>
2014-10-19 19:48:01 -0500

    first

---
+++ content/test.md
@@ -0,0 +1,9 @@
+Title: My super title2
+Date: 2010-12-03 10:20
+Tags: thats, awesome
+Category: yeah
+Slug: my-super-post
+Author: Alexis Metaireau
+Summary: Short version for index and feeds
+
+{% notebook lovecraft.ipynb %}
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ryan Wheeler'
+SITENAME = u'Broken Data'
+SITEURL = ''
+
+PATH = 'content'
+TIMEZONE = 'Europe/Paris'
+
+
+DEFAULT_LANG = u'en'
+
+THEME = 'pelican-bootstrap3'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+NOTEBOOK_DIR = '/home/ryan/site/brokendata/notebooks/'
+DEFAULT_PAGINATION = 10
+PLUGIN_PATHS = 'pelican-plugins'
+PLUGINS = ['liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.youtube', 'liquid_tags.vimeo',
+           'liquid_tags.include_code', 'liquid_tags.notebook']
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+BOOTSTRAP_THEME='superhero' #nice but, background doesn't work well with code as is
